c284605724b54982ccc2a6ce08dfebd06368e80b
Pierre Nel <pierrenel@users.noreply.github.com>
2021-09-27 06:30:03 +0200

    extra bits via dom

--- config.el
+++ config.el
@@ -22,6 +22,31 @@
 
 (setq-default line-spacing 0.5)
 
+(setq js-indent-level 2
+      js2-basic-offset 2)
+
+(setq +set-eslint-checker nil)
+(after! lsp-ui
+  ;; for whatever reason, this was running twice.
+  (setq lsp-ui-sideline-show-hover t)
+  (when (not +set-eslint-checker)
+    (progn
+      (setq +set-eslint-checker t)
+      (flycheck-add-mode 'javascript-eslint 'web-mode)
+      (flycheck-add-next-checker 'lsp-ui '(warning . javascript-eslint)))))
+
+(after! web-mode
+  (add-hook 'web-mode-hook #'flycheck-mode)
+
+  (setq web-mode-markup-indent-offset 2 ;; Indentation
+        web-mode-code-indent-offset 2
+        web-mode-enable-auto-quoting nil ;; disbale adding "" after an =
+        web-mode-auto-close-style 2))
+
+(after! web-mode
+  (remove-hook 'web-mode-hook #'+javascript-init-lsp-or-tide-maybe-h)
+  (add-hook 'web-mode-local-vars-hook #'+javascript-init-lsp-or-tide-maybe-h))
+
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
@@ -31,9 +56,9 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-;; (setq doom-theme 'doom-moonlight)
+(setq doom-theme 'doom-one)
 ;; (load-theme 'night-owl t)
-(load-theme 'exotica t)
+;; (load-theme 'exotica t)
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
